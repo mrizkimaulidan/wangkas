cbacc939e21621fd2c7b604a650692ae43673a6c
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-01-30 14:20:10 +0800

    set up user factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(UsersSeeder::class);
         $this->call(SchoolClassesSeeder::class);
         $this->call(SchoolMajorsSeeder::class);
     }
