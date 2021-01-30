f37299855242f12d6c17f228b67c73e194b189fb
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-01-30 22:05:54 +0800

    change column

--- database/migrations/2021_01_30_213027_create_students_table.php
+++ database/migrations/2021_01_30_213027_create_students_table.php
@@ -19,7 +19,8 @@ class CreateStudentsTable extends Migration
             $table->string('email')->unique();
             $table->string('phone_number')->unique();
             $table->tinyInteger('gender');
-            $table->integer('school_year');
+            $table->integer('school_year_start');
+            $table->integer('school_year_end');
             $table->timestamps();
 
             $table->foreignId('school_class_id')->constrained();
