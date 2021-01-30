e668c5a5cc6d08588e8446819d1beb013300fd33
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-01-30 21:35:00 +0800

    added students migration

---
+++ database/migrations/2021_01_30_213027_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->tinyInteger('gender');
+            $table->string('school_year');
+            $table->timestamps();
+
+            $table->foreignId('school_class_id')->constrained();
+            $table->foreignId('school_major_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
