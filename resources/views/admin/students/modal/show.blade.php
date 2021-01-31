3acbae4159fa112de7c7d05ad0373482ae8d3986
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-01-31 16:32:17 +0800

    clear input when button onclick

--- resources/views/admin/students/modal/show.blade.php
+++ resources/views/admin/students/modal/show.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Detail Siswa</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close clear-input" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="row">
@@ -68,7 +68,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                <button type="button" class="btn btn-secondary clear-input" data-bs-dismiss="modal">Tutup</button>
             </div>
         </div>
     </div>
