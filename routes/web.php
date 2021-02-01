643ee1e27bfae7920cd8605d8560dd41904dd64e
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-02-01 18:03:19 +0800

    change to dashbaord controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdministratorController;
 use App\Http\Controllers\API\v1\SchoolClassController as V1SchoolClassController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SchoolClassController;
 use App\Http\Controllers\SchoolMajorController;
 use App\Http\Controllers\StudentController;
@@ -22,9 +23,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 Route::name('admin.')->group(function () {
     Route::resource('siswa', StudentController::class);
