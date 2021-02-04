e072365bc5b852a6c2bb2375e2a6103048be9419
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-02-05 01:33:25 +0800

    change the text from Ubah to Simpan

---
+++ resources/views/admin/administrator/modal/edit.blade.php
@@ -0,0 +1,38 @@
+<div class="modal fade" id="editAdministratorModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Detail Administrator</h5>
+                <button type="button" class="btn-close clear-input" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="#" method="post" id="administrator-edit-form">
+                    @csrf @method('PUT')
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Nama Lengkap</label>
+                                <input type="text" class="form-control" name="name" id="name">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" name="email" id="email">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary clear-input"
+                            data-bs-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
