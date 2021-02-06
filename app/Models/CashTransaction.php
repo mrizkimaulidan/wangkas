28d5e24fa5ef888ee16f7d195f2a3816cab5fe77
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-02-06 14:36:10 +0800

    student name on index

--- app/Models/CashTransaction.php
+++ app/Models/CashTransaction.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CashTransaction extends Model
 {
     use HasFactory;
+
+    public function students()
+    {
+        return $this->belongsTo(Student::class, 'student_id', 'id');
+    }
 }
