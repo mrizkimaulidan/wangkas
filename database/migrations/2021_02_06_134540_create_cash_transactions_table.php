fb525618843c0da03a16fa0b880221eac7da2d89
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-02-06 13:47:27 +0800

    cash transaction migration and model

---
+++ database/migrations/2021_02_06_134540_create_cash_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCashTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cash_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('amount');
+            $table->boolean('is_paid');
+            $table->string('day');
+            $table->string('month');
+            $table->integer('year');
+            $table->text('note')->nullable();
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('student_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cash_transactions');
+    }
+}
