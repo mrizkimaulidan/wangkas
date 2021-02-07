12da895868a79c8f06747107ffe5e01bc969bdbf
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-02-07 14:21:45 +0800

    customize index cash transaction

--- resources/views/admin/cash_transactions/index.blade.php
+++ resources/views/admin/cash_transactions/index.blade.php
@@ -5,6 +5,172 @@
     {{-- Start Statistics --}}
     <div class="col-6 col-lg-3 col-md-6">
         <div class="card">
+            <div class="card-body px-3 py-4-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="stats-icon green">
+                            <i class="iconly-boldPaper-Plus"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h6 class="text-muted font-semibold">Lunas</h6>
+                        <h6 class="font-extrabold mb-0">
+                            1</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
+            <div class="card-body px-3 py-4-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="stats-icon red">
+                            <i class="iconly-boldPaper-Fail"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h6 class="text-muted font-semibold">Belum Lunas</h6>
+                        <h6 class="font-extrabold mb-0">
+                            1</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
+            <div class="card-body px-3 py-4-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="stats-icon">
+                            <i class="iconly-boldChart"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h6 class="text-muted font-semibold">Total Bulan Ini</h6>
+                        <h6 class="font-extrabold mb-0">
+                            1</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
+            <div class="card-body px-3 py-4-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="stats-icon">
+                            <i class="iconly-boldChart"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h6 class="text-muted font-semibold">Total Tahun Ini</h6>
+                        <h6 class="font-extrabold mb-0">
+                            1</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-6 col-lg-6 col-md-6">
+        <div class="card">
+            <div class="card-body px-3 py-4-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="stats-icon green">
+                            <i class="iconly-boldActivity"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h6 class="text-muted font-semibold">Sudah Membayar</h6>
+                        <h6 class="font-extrabold mb-0">
+                            1</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-6 col-lg-6 col-md-6">
+        <div class="card">
+            <div class="card-body px-3 py-4-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="stats-icon red">
+                            <i class="iconly-boldActivity"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h6 class="text-muted font-semibold">Belum Membayar</h6>
+                        <h6 class="font-extrabold mb-0">
+                            1</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-lg-12 col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>Belum Membayar Bulan Ini</h4>
+            </div>
+            <div class="card-content pb-4">
+                <div class="row">
+                    <div class="col-6 col-lg-6 col-md-6">
+                        <div class="recent-message d-flex px-4 py-3">
+                            <div class="name ms-4">
+                                <h5 class="mb-1">Buday</h5>
+                                <h6 class="text-muted mb-0">0078463816</h6>
+                            </div>
+                        </div>
+                        <div class="recent-message d-flex px-4 py-3">
+                            <div class="name ms-4">
+                                <h5 class="mb-1">Buday</h5>
+                                <h6 class="text-muted mb-0">0078463816</h6>
+                            </div>
+                        </div>
+                        <div class="recent-message d-flex px-4 py-3">
+                            <div class="name ms-4">
+                                <h5 class="mb-1">Buday</h5>
+                                <h6 class="text-muted mb-0">0078463816</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-6 col-md-6">
+                        <div class="recent-message d-flex px-4 py-3">
+                            <div class="name ms-4">
+                                <h5 class="mb-1">Buday</h5>
+                                <h6 class="text-muted mb-0">0078463816</h6>
+                            </div>
+                        </div>
+                        <div class="recent-message d-flex px-4 py-3">
+                            <div class="name ms-4">
+                                <h5 class="mb-1">Buday</h5>
+                                <h6 class="text-muted mb-0">0078463816</h6>
+                            </div>
+                        </div>
+                        <div class="recent-message d-flex px-4 py-3">
+                            <div class="name ms-4">
+                                <h5 class="mb-1">Buday</h5>
+                                <h6 class="text-muted mb-0">0078463816</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-4">
+                    <button class='btn btn-block btn-xl btn-light-primary font-bold mt-3'>Lihat Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
             <div class="card-body px-3 py-4-5">
                 <div class="row">
                     <div class="col-md-4">
@@ -144,7 +310,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     {{-- End of Statistics --}}
 
     @include('utilities.alert-flash-message')
