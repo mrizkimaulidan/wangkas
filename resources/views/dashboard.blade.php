e0a3df73515d01352bd71229428a0e96b6a73e41
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-02-14 18:16:23 +0800

    fix bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <div class="col-12 col-lg-12">
         <div class="row">
             <div class="col-6 col-lg-3 col-md-6">
-                <a href="{{ route('siswa.index') }}">
+                <a href="{{ route('pelajar.index') }}">
                     <div class="card">
                         <div class="card-body px-3 py-4-5">
                             <div class="row">
