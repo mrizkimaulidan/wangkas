e15d239ffcf54741aedfca594e4db16ca6c1189b
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-02-18 18:29:06 +0800

    change login message when fail

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Email atau password tidak terdaftar!',
+    'password' => 'Password yang dimasukkan salah!',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
