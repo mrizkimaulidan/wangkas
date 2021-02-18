14c8a1cb6c61fd10ad205cd7183aa1a5b5ef5572
novilfahlevy <novilfreon@gmail.com>
2021-02-18 20:51:06 +0800

    fix report table

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -109,21 +109,24 @@
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Nama Pelajar</th>
-                        <th scope="col">Total Bayar</th>
                         <th scope="col">Tanggal</th>
                         <th scope="col">Status</th>
                         <th scope="col">Telah Membayar Bulan Ini</th>
-                        <th scope="col">Total</th>
+                        <th scope="col">Nominal Bayar</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="row">#</th>
                         <td>Muhammad Rizki Maulidan</td>
-                        <td>Rp10,000.00</td>
                         <td>18-02-2021</td>
                         <td>Lunas</td>
                         <td>4x</td>
+                        <td>Rp10,000.00</td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" align="right"><b>Total</b></td>
+                        <td>Rp10,000.00</td>
                     </tr>
                 </tbody>
             </table>
