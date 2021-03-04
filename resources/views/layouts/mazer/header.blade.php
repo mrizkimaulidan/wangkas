43336740b307e6d8aaae0187395eda6345f68b7f
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-03-04 13:23:24 +0800

    flatpickr integration

--- resources/views/layouts/mazer/header.blade.php
+++ resources/views/layouts/mazer/header.blade.php
@@ -21,5 +21,6 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link rel="shortcut icon" href="{{ asset('images/favicon.svg" type="image/x-icon') }}">
 </head>
