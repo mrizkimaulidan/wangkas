32ae2b9057a3091cb58da243adf13c373be3efe1
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-03-07 12:59:50 +0800

    remove slash

--- routes/export.php
+++ routes/export.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\Export\CashTransactionController;
 use App\Http\Controllers\Export\CashTransactionReportController;
 
 Route::get('/laporan/filter/export/{start_date}/{end_date}', CashTransactionReportController::class)->name('laporan.export');
-Route::get('/pelajar/export/', StudentController::class)->name('pelajar.export');
+Route::get('/pelajar/export', StudentController::class)->name('pelajar.export');
 Route::get('/kas/export', CashTransactionController::class)->name('kas.export');
