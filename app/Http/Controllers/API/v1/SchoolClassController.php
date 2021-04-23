2adc1947a02d02d3e57f5eb78c7263f848d4b354
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-23 23:08:11 +0800

    invokable school class api show

--- app/Http/Controllers/API/v1/SchoolClassController.php
+++ app/Http/Controllers/API/v1/SchoolClassController.php
@@ -4,19 +4,21 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\SchoolClass;
-use App\Repositories\SchoolClassRepository;
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SchoolClassController extends Controller
 {
-    public function __construct(
-        private SchoolClassRepository $schoolClassRepository
-    ) {
-    }
-
-    public function show(string $id)
+    /**
+     * Handle the incoming request.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(string $id)
     {
-        return response()->json(['status' => Response::HTTP_OK, 'data' => $this->schoolClassRepository->findSchoolClass($id)], Response::HTTP_OK);
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'message' => 'Data berhasil diambil!',
+            'data' => SchoolClass::select('id', 'name')->findOrFail($id)
+        ], Response::HTTP_OK);
     }
 }
