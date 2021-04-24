41bcf3e29d98d90af54a0745ff4bf0b2861fc4f9
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-24 12:08:05 +0800

    meta csp

--- resources/views/layouts/mazer/header.blade.php
+++ resources/views/layouts/mazer/header.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
     <title>{{ $title }} - {{ config('app.name') }}</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
