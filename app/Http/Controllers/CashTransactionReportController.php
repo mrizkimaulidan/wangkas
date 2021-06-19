5ba3d180eac8572fb185664100b670c4145f79e7
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-06-20 01:00:02 +0800

    minifying code

--- app/Http/Controllers/CashTransactionReportController.php
+++ app/Http/Controllers/CashTransactionReportController.php
@@ -19,17 +19,13 @@ class CashTransactionReportController extends Controller
      */
     public function index(Request $request)
     {
-        $sum_this_day = $this->cashTransactionReportRepository->sumAmount('amount', 'this_day');
-        $sum_this_week = $this->cashTransactionReportRepository->sumAmount('amount', 'this_week');
-        $sum_this_month = $this->cashTransactionReportRepository->sumAmount('amount', 'this_month');
-        $sum_this_year = $this->cashTransactionReportRepository->sumAmount('amount', 'this_year');
         $filtered_results = $this->cashTransactionReportRepository->filter();
 
         return view('reports.index', [
-            'sum_this_day' => $sum_this_day,
-            'sum_this_week' => $sum_this_week,
-            'sum_this_month' => $sum_this_month,
-            'sum_this_year' => $sum_this_year,
+            'sum_this_day' => $this->cashTransactionReportRepository->sumAmount('amount', 'this_day'),
+            'sum_this_week' => $this->cashTransactionReportRepository->sumAmount('amount', 'this_week'),
+            'sum_this_month' => $this->cashTransactionReportRepository->sumAmount('amount', 'this_month'),
+            'sum_this_year' => $this->cashTransactionReportRepository->sumAmount('amount', 'this_year'),
             'reports_data' => $filtered_results['filtered_data'] ?? [],
             'total_amount_is_paid' => $filtered_results['total_amount']['is_paid'] ?? 0,
             'total_amount_is_not_paid' => $filtered_results['total_amount']['is_not_paid'] ?? 0,
