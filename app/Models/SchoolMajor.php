1b3a6dee845b83f01187b201e855f9d82b26cf93
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-06-19 18:47:48 +0800

    implement soft delete migration on school major

--- app/Models/SchoolMajor.php
+++ app/Models/SchoolMajor.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\Student;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SchoolMajor extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = ['created_at', 'updated_at'];
 
