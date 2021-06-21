1e3fc4e6d33d61cdc3fd4aeeae0ef4449bb25a50
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-06-21 16:28:56 +0800

    remove comments

--- routes/api.php
+++ routes/api.php
@@ -8,21 +8,6 @@ use App\Http\Controllers\API\v1\CashTransactionController;
 use App\Http\Controllers\API\v1\DashboardChartController;
 use App\Http\Controllers\API\v1\StudentController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::name('api.')->prefix('v1')->group(function () {
     Route::get('/kelas/{id}', SchoolClassController::class)->name('kelas.show');
     Route::get('/pelajar/{id}', StudentController::class)->name('pelajar.show');
--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,6 @@ use App\Http\Controllers\SchoolClassHistoryController;
 use App\Http\Controllers\SchoolClassSoftDeleteController;
 use App\Http\Controllers\SchoolMajorHistoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 require __DIR__ . '/auth.php';
 
 Route::middleware('auth')->group(function () {
