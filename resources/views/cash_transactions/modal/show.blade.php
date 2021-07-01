fc36ad05fda2a3d2d6154d849b8415d576e82bbd
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-07-02 00:03:21 +0800

    change title

--- resources/views/cash_transactions/modal/show.blade.php
+++ resources/views/cash_transactions/modal/show.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Tambah Data Kas</h5>
+                <h5 class="modal-title">Detail Kas</h5>
                 <button type="button" class="btn-close clear-input" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
