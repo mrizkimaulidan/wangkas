878837fb15f4ca21c3b83fa56de8064347e8bfc9
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-07-21 18:03:57 +0800

    remove console log

--- resources/views/cash_transactions/script.blade.php
+++ resources/views/cash_transactions/script.blade.php
@@ -42,7 +42,6 @@
     $.ajax({
         url: url,
         success: function(data) {
-            console.log(data);
                 $('#editCashTransactionModal input').prop('disabled', false);
                 $('#editCashTransactionModal textarea').prop('disabled', false);
                 $('#editCashTransactionModal select').prop('disabled', false);
