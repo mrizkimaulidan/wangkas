ba725e18e1417e04b26d979d02158048775f3349
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-07-30 04:28:35 +0800

    update student request validation

--- app/Http/Requests/StudentUpdateRequest.php
+++ app/Http/Requests/StudentUpdateRequest.php
@@ -24,15 +24,15 @@ class StudentUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'student_identification_number' => 'required|numeric|digits_between:3, 191',
-            'name' => 'required|min:3|max:191',
-            'gender' => 'required|max:191',
-            'school_class_id' => 'required|max:191',
-            'school_major_id' => 'required|max:191',
-            'email' => 'required|email|max:191',
-            'phone_number' => 'required|numeric|digits_between:3, 191',
-            'school_year_start' => 'required|numeric|digits_between:3, 191',
-            'school_year_end' => 'required|numeric|digits_between:3, 191'
+            'student_identification_number' => ['required', 'min:3', 'max:191'],
+            'name' => ['required', 'min:3', 'max:191'],
+            'gender' => ['required'],
+            'school_class_id' => ['required'],
+            'school_major_id' => ['required'],
+            'email' => ['required', 'email', 'max:191'],
+            'phone_number' => ['required', 'min:3', 'max:191'],
+            'school_year_start' => ['required'],
+            'school_year_end' => ['required']
         ];
     }
 
@@ -40,37 +40,30 @@ class StudentUpdateRequest extends FormRequest
     {
         return [
             'student_identification_number.required' => 'Kolom nis/nisn wajib diisi!',
-            'student_identification_number.numeric' => 'Kolom nis/nisn harus angka!',
-            'student_identification_number.digit_between' => 'Kolom nis/nisn maksimal 191 karakter!',
+            'student_identification_number.min' => 'Kolom nis/nisn minimal :min karakter!',
+            'student_identification_number.max' => 'Kolom nis/nisn maksimal :max karakter!!',
 
             'name.required' => 'Kolom nama lengkap wajib diisi!',
-            'name.min' => 'Kolom nama lengkap minimal 3 karakter!',
-            'name.max' => 'Kolom nama lengkap maksimal 191 karakter!',
+            'name.min' => 'Kolom nama lengkap minimal :min karakter!',
+            'name.max' => 'Kolom nama lengkap maksimal :max karakter!!',
 
             'gender.required' => 'Kolom jenis kelamin wajib diisi!',
-            'gender.max' => 'Kolom jenis kelamin maksimal 191 karakter!',
 
             'school_class_id.required' => 'Kolom kelas wajib diisi!',
-            'school_class_id.max' => 'Kolom kelas maksimal 191 karakter!',
 
             'school_major_id.required' => 'Kolom jurusan wajib diisi!',
-            'school_major_id.max' => 'Kolom jurusan maksimal 191 karakter!',
 
             'email.required' => 'Kolom email wajib diisi!',
             'email.email' => 'Kolom email harus email yang valid!',
-            'email.max' => 'Kolom email maksimal 191 karakter!',
+            'email.max' => 'Kolom email maksimal :max karakter!',
 
             'phone_number.required' => 'Kolom nomor handphone wajib diisi!',
-            'phone_number.numeric' => 'Kolom nomor handphone harus angka!',
-            'phone_number.digit_between' => 'Kolom nomor handphone maksimal 191 karakter!',
+            'phone_number.min' => 'Kolom nomor handphone :min karakter!',
+            'phone_number.max' => 'Kolom nomor handphone :max karakter!',
 
             'school_year_start.required' => 'Kolom awal tahun ajaran wajib diisi!',
-            'school_year_start.numeric' => 'Kolom awal tahun ajaran harus angka!',
-            'school_year_start.digit_between' => 'Kolom awal tahun ajaran maksimal 191 karakter!',
 
             'school_year_end.required' => 'Kolom akhir tahun ajaran wajib diisi!',
-            'school_year_end.numeric' => 'Kolom akhir tahun ajaran harus angka!',
-            'school_year_end.digit_between' => 'Kolom akhir tahun ajaran maksimal 191 karakter!',
         ];
     }
 }
