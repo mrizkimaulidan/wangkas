a3cb7e050a89e4f3fa3d47aec645b3bd2b5bf410
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-07-31 22:23:48 +0800

    remove unused helper

--- app/helper.php
+++ app/helper.php
@@ -1,22 +1,5 @@
 <?php
 
-if (!function_exists('count_data')) {
-    /**
-     * Hitung seluruh data, jika data kosong return 0. Jika data ada, maka hitung menggunakan method count().
-     *
-     * @param Object $data
-     * @return Integer
-     */
-    function count_data(Object $data): Int
-    {
-        if ($data->isEmpty()) {
-            return 0;
-        }
-
-        return $data->count();
-    }
-}
-
 if (!function_exists('indonesian_currency')) {
     /**
      * Ubah mata uang menjadi format indonesia
