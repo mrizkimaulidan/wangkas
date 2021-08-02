ab296a561a954f997ecc46c6e65711d5b6981f2a
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-08-02 18:41:16 +0800

    tidy code

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,12 +17,15 @@ class DashboardController extends Controller
 
     public function __invoke(): View
     {
+        $cash_transaction_this_month = indonesian_currency($this->cashTransactionRepository->sumAmountBy('month', month: date('m')));
+        $latest_cash_transactions_by_limit = $this->cashTransactionRepository->cashTransactionLatest(['student_id', 'user_id', 'bill', 'amount', 'date'], 5);
+
         return view('dashboard.index', [
             'student_count' => Student::count(),
             'school_class_count' => SchoolClass::count(),
             'school_major_count' => SchoolMajor::count(),
-            'cash_transaction_this_month' => indonesian_currency($this->cashTransactionRepository->sumAmountBy('month', month: date('m'))),
-            'latest_cash_transactions_by_limit' => $this->cashTransactionRepository->cashTransactionLatest(['student_id', 'user_id', 'bill', 'amount', 'date'], 5)
+            'cash_transaction_this_month' => $cash_transaction_this_month,
+            'latest_cash_transactions_by_limit' => $latest_cash_transactions_by_limit
         ]);
     }
 }
