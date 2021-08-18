3c2ac74eebb445498fe650e7309a9d099c12f7d8
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-08-18 18:13:33 +0800

    remove line whitespace

--- app/Repositories/DashboardChartRepository.php
+++ app/Repositories/DashboardChartRepository.php
@@ -21,7 +21,6 @@ class DashboardChartRepository extends Controller
     {
         $months = ['jan', 'feb', 'mar', 'apr', 'mei', 'jun', 'jul', 'agu', 'sep', 'okt', 'nov', 'des'];
 
-
         for ($i = 1; $i <= 12; $i++) {
             // Looping dari angka 1-12 karena setiap tahun ada 12 bulan dan menghitung kolom amount berdasarkan bulannya
             $cash_transactions = $this->model
