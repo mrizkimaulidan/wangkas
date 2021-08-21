8df80c85b6521f8e169848e489b43460e2adbb09
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-08-21 15:02:38 +0800

    change title

--- resources/views/administrators/modal/edit.blade.php
+++ resources/views/administrators/modal/edit.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Detail Administrator</h5>
+                <h5 class="modal-title">Ubah Administrator</h5>
                 <button type="button" class="btn-close clear-input" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
