0f20030ddff5b02860475e1b47d5437f6ef68757
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-09-14 23:07:11 +0800

    change button title and bootstrap class

--- resources/views/cash_transactions/modal/edit.blade.php
+++ resources/views/cash_transactions/modal/edit.blade.php
@@ -74,7 +74,7 @@
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary clear-input"
                             data-bs-dismiss="modal">Tutup</button>
-                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <button type="submit" class="btn btn-success">Ubah</button>
                     </div>
                 </form>
             </div>
