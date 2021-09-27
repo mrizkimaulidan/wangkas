d153ae00b6d2ed36be32efb9d6bceeff79e0d427
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-09-28 01:35:38 +0800

    modal dialog scrollable

--- resources/views/cash_transactions/modal/create.blade.php
+++ resources/views/cash_transactions/modal/create.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="addCashTransactionModal" data-bs-backdrop="static" data-bs-keyboard="false"
     aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Tambah Data Kas</h5>
