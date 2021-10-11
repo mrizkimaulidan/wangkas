c334eeee9fa9341d40531927d667971848eaf9aa
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-10-11 22:51:29 +0800

    add

--- resources/views/cash_transactions/filter/index.blade.php
+++ resources/views/cash_transactions/filter/index.blade.php
@@ -4,7 +4,7 @@
 <section class="row">
 
     <div class="card px-3 py-3">
-        <label for="start_date" class=" fw-bold pb-3">Filter Data dengan Rentang Tanggal</label>
+        <label for="start_date" class=" fw-bold pb-3">Filter Data dengan Rentang Tanggal :</label>
         <div class="input-group">
             <input type="date" name="start_date" class="form-control" id="start_date"
                 placeholder="Pilih tanggal awal..">
