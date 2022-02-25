0f8e95d859eed8321746231a93408a3233a7de65
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-02-25 20:23:43 +0800

    footer dynamic year

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2021 &copy; Mazer</p>
+            <p>{{ date('Y') }} &copy; Mazer</p>
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
