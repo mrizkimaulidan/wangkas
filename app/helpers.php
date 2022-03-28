b64ce3ee80b9e6ccd7e2b8de2dc006232c62ab2b
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-03-28 20:18:18 +0800

    feat: remove unused global helper

--- app/helpers.php
+++ app/helpers.php
@@ -12,18 +12,3 @@ if (!function_exists('indonesian_currency')) {
         return 'Rp' . number_format((int) $number, 2, ',',);
     }
 }
-
-if (!function_exists('get_gender_name')) {
-    /**
-     * Mengubah angka menjadi nama jenis kelamin.
-     * 1 = Laki-laki
-     * 2 = Perempuan
-     *
-     * @param Integer $value
-     * @return String
-     */
-    function get_gender_name(int $value): String
-    {
-        return $value === 1 ? 'Laki-laki' : 'Perempuan';
-    }
-}
