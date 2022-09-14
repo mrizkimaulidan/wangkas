68a540e28851fc7a9670992e1e1c1e928d3be7f4
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-09-14 08:34:57 +0800

    chore: add comment

--- app/Contracts/ApiInterface.php
+++ app/Contracts/ApiInterface.php
@@ -4,6 +4,9 @@ namespace App\Contracts;
 
 use Illuminate\Http\JsonResponse;
 
+/**
+ * An interface for REST API feature.
+ */
 interface ApiInterface
 {
     public function show(int $id): JsonResponse;
