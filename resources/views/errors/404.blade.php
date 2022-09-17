dab5d1761937b51d88c123d922a098c61a1dbdad
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-09-17 16:08:09 +0800

    fix: incorrect 404 image page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -16,7 +16,7 @@
 	<div id="error">
 		<div class="error-page container">
 			<div class="col-md-8 col-12 offset-md-2">
-				<img class="img-error" src="{{ asset('images/errors/404.svg') }}" alt="Not Found">
+				<img class="img-error" src="{{ asset('img/errors/404.svg') }}" alt="Not Found">
 				<div class="text-center">
 					<h1 class="error-title">NOT FOUND</h1>
 					<p class='fs-5 text-gray-600'>The page you are looking not found.</p>
