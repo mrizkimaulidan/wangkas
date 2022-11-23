347f1248b7037529b2e3ad902b414e7cdd60b18f
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-11-23 19:42:07 +0800

    chore: rename file

--- app/Contracts/APIInterface.php
+++ app/Contracts/APIInterface.php
@@ -7,7 +7,7 @@ use Illuminate\Http\JsonResponse;
 /**
  * Interface untuk fitur REST API.
  */
-interface ApiInterface
+interface APIInterface
 {
     public function show(int $id): JsonResponse;
     public function edit(int $id): JsonResponse;
