7b9a3cab3e7e2bcce8d026a0591392ca8cc96656
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-06-25 20:30:57 +0800

    config: :wrench: fix telescope config

--- config/app.php
+++ config/app.php
@@ -167,8 +167,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
+        App\Providers\RouteServiceProvider::class
     ])->toArray(),
 
     /*
