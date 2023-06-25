b284566ae91472db4f8eef3494d85338d9ade1f4
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-06-25 20:48:08 +0800

    feat: :sparkles: set up school class module

---
+++ database/seeders/SchoolClassSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SchoolClass;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SchoolClassSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $schoolClasses = ['DKV', 'TJKT', 'PPLG'];
+
+        foreach ($schoolClasses as $schoolClass) {
+            for ($i = 1; $i <= 3; $i++) {
+                SchoolClass::create([
+                    'name' => "$schoolClass $i"
+                ]);
+            }
+        }
+    }
+}
