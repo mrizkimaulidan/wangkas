ed8eb62e797891a8ce0989ce507a64e3fed456ae
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-06-25 21:42:05 +0800

    style: :art: customize navigation

--- resources/views/school_classes/index.blade.php
+++ resources/views/school_classes/index.blade.php
@@ -5,14 +5,18 @@
 	<div class="page-title">
 		<div class="row">
 			<div class="col-12 col-md-6 order-md-1 order-last">
-				<h3>Dashboard</h3>
-				<p class="text-subtitle text-muted">Dashboard.</p>
+				<h3>Data Kelas</h3>
+				<p class="text-subtitle text-muted">Halaman untuk manajemen data kelas seperti melihat, mengubah dan menghapus.
+				</p>
 			</div>
 			<div class="col-12 col-md-6 order-md-2 order-first">
 				<nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
 					<ol class="breadcrumb">
 						<li class="breadcrumb-item">
 							<a href="{{ route('dashboard') }}">Dashboard</a>
+						</li>
+						<li class="breadcrumb-item active" aria-current="page">
+							Data Kelas
 						</li>
 					</ol>
 				</nav>
