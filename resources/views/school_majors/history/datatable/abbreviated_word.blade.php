dd7d2fb693c1e5334d72d6a201a03ba245af8d4a
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-06-25 19:58:40 +0800

    changes: removing all contents

--- resources/views/school_majors/history/datatable/abbreviated_word.blade.php
+++
@@ -1,3 +0,0 @@
-<span class="badge w-100 rounded-pill bg-primary">
-    {{ $model->abbreviated_word }}
-</span>
--- resources/views/students/datatable/school_major.blade.php
+++
@@ -1,3 +0,0 @@
-<span class="badge w-100 rounded-pill bg-success" data-bs-toggle="tooltip" data-placement="top" title="{{ $model->school_major->name }}">
-    {{ $model->school_major->abbreviated_word }}
-</span>
