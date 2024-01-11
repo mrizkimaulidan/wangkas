eb5ce4fc908d21f3eba4231ea7d808c7fe1cf263
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-01-11 19:17:20 +0800

    ui: :lipstick: hardcode app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
 				<div class="sidebar-header position-relative">
 					<div class="d-flex justify-content-between align-items-center">
 						<div class="logo">
-							<a href="index.html"><img src="{{ asset('compiled/svg/logo.svg') }}" alt="Logo" srcset="" /></a>
+							<a href="{{ route('dashboard') }}">Wangkas</a>
 						</div>
 						<div class="theme-toggle d-flex gap-2 align-items-center mt-2">
 							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
