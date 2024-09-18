033e5b710821bedb37446f8752d994dd90d03406
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-09-19 01:28:40 +0800

    fix: :bug: missing update profile route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Livewire\Dashboard;
 use App\Livewire\SchoolClasses\SchoolClassTable;
 use App\Livewire\SchoolMajors\SchoolMajorTable;
 use App\Livewire\Students\StudentTable;
+use App\Livewire\UpdateProfile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
@@ -19,4 +20,5 @@ Route::get('/dashboard', Dashboard::class)->name('dashboard');
 Route::get('/kelas', SchoolClassTable::class)->name('school-classes.index');
 Route::get('/jurusan', SchoolMajorTable::class)->name('school-majors.index');
 Route::get('/pengguna', AdministratorTable::class)->name('administrators.index');
+Route::get('/profil', UpdateProfile::class)->name('update-profiles.index');
 Route::get('/pelajar', StudentTable::class)->name('students.index');
