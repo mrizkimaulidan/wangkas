2a407b1925e0935433bc3b3260ab51d29e1dc99a
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-10-21 16:19:52 +0800

    refactor: :recycle: use Number class on local amount format helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,13 @@
 <?php
 
+use Illuminate\Support\Number;
+
 /**
  * Format a number as a local currency amount.
  */
 if (! function_exists('local_amount_format')) {
-    function local_amount_format(float $num): string
+    function local_amount_format(int|float $num): string
     {
-        return 'Rp'.number_format($num, 0, ',', '.');
+        return Number::currency($num, in: 'IDR', locale: 'id');
     }
 }
